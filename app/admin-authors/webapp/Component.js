b6f59a4c892857b70de8c61b815739a07c04858e
Antal Perger <antal.perger@sap.com>
2026-02-20 10:58:22 +0100

    cap nodejs project created

---
+++ app/admin-authors/webapp/Component.js
@@ -0,0 +1,3 @@
+sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("bookshop8.admin-authors.Component", {
+  metadata:{ manifest: 'json' }
+}))
---
+++ app/browse/webapp/Component.js
@@ -0,0 +1,3 @@
+sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("bookshop8.browse.Component", {
+  metadata:{ manifest:'json' }
+}))
